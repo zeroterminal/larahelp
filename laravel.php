d8179a4d62d7b6ddd35305631680bf16b4f01e09
Jay <zerolinuxx@gmail.com>
2019-07-12 14:37:49 +0300

    Create laravel.php

---
+++ laravel.php
@@ -0,0 +1,14 @@
+### Voyager Check Box ###
+#########################
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Resizable;
+
+class ClassName extends Model
+{
+    use Resizable;
+}
